9aafec469861f869b9edd2273f8a6f9c2f136ced
auangerv <auangerv@miage21a17.dmiage.u-paris10.fr>
2014-03-12 14:43:58 +0100

    création carte Stage

---
+++ src/java/fr/p10/mi/classe/Stage.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.p10.mi.classe;
+
+/**
+ *
+ * @author auangerv
+ */
+public class Stage {
+    
+}
